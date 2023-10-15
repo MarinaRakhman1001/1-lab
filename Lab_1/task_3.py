ad1bafc3a539634c8b07ed773d3c0098ace0e9e3
MarinaRakhman1001 <147854714+MarinaRakhman1001@users.noreply.github.com>
2023-10-15 23:22:35 +0300

    Add files via upload

---
+++ Lab_1/task_3.py
@@ -0,0 +1,10 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+x = len(list_players)
+kom_1 = list_players[:3]
+kom_2 = list_players[3:]
+
+# TODO Разделите участников на две команды
+
+print( kom_1)
+print( kom_2)
---
+++ Lab_1/task_4.py
@@ -0,0 +1,11 @@
+users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
+
+# TODO Добавьте словарь и замените в нем нулевые значения статисчикой посещений
+
+Statistic = {
+    'Общее количество': len(users),
+    'Уникальные посещения': len(set(users))
+}
+print(Statistic)
+
+
